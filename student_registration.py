c9ca7970c28755410ae7dcb9081b17f7dd1c4b20
Abdul latif <iamabdullatifmo@gmail.com>
2026-09-11 09:30:11 -0700

    render changes

--- student_registration.py
+++ student_registration.py
@@ -5,46 +5,84 @@ from flask_mail import Message
 
 register_student_bp = Blueprint("register_student",__name__)
 
-
-@register_student_bp.route("/registerStudent",methods=["POST","GET"])
+@register_student_bp.route("/registerStudent", methods=["POST", "GET"])
 def register_student():
     if request.method == "POST":
-        first_N=request.form.get("first")
-        last_N=request.form.get("last")
-        email=request.form.get("email")
-        full_N=first_N + " " + last_N
+        first_N = request.form.get("first")
+        last_N = request.form.get("last")
+        email = request.form.get("email")
         department = request.form.get("department")
         years_of_study = int(request.form.get("years_of_study"))
 
+        full_N = first_N + " " + last_N
         level = 100
-    
-        user = Student(first_N=first_N,last_N=last_N,email=email,full_Name=full_N,department=department)
+
+        # Create student
+        user = Student(
+            first_N=first_N,
+            last_N=last_N,
+            email=email,
+            full_Name=full_N,
+            department=department
+        )
+
         db.session.add(user)
         db.session.commit()
 
-        msg = Message(subject='School Mangement',recipients=[user.email] )
-        msg.html = f"""
-                    Hi {user.full_Name},<br>
-                    Welcome to TaTU institution.<br>
-                    <p>This is your temporary password: {user.password}</p>
-                    <p>This is your ID: {user._id}</p>
-                    <p>Login with the ID and the password and change the password with your preferred password</p>
+        # Create YearsOfStudy record
+        years = YearsOfStudy(
+            student_id=user._id,
+            years_of_study=years_of_study,
+            semester=1,
+            level=level
+        )
 
-                    """
-        mail.send(msg)
-        flash("Registration was successfully.Login with the ID and password given to you and change the password")
-        # query and check the department table whose department name matches with the department the student chooses
-        dept = Department.query.filter_by(dept_name=user.department).first()
-        student_id = session.get("student_id")
-        years= YearsOfStudy(student_id=user._id,years_of_study=years_of_study,semester=1,level = level)
         db.session.add(years)
         db.session.commit()
-        # Store the department _id as the same of student _id
+
+        # Update department
+        dept = Department.query.filter_by(
+            dept_name=user.department
+        ).first()
+
         if dept:
-         dept._id = user._id
-         db.session.commit()
-        
+            dept._id = user._id
+            db.session.commit()
+
+        # Send email separately
+        try:
+            msg = Message(
+                subject="School Management",
+                recipients=[user.email]
+            )
+
+            msg.html = f"""
+            Hi {user.full_Name},<br><br>
+
+            Welcome to TaTU institution.<br><br>
+
+            <p>This is your temporary password: {user.password}</p>
+            <p>This is your ID: {user._id}</p>
+
+            <p>
+            Login with the ID and password given to you
+            and change the password to your preferred password.
+            </p>
+            """
+
+            mail.send(msg)
+
+            print("Registration email sent successfully")
+
+        except Exception as e:
+            print("EMAIL ERROR:", repr(e))
+
+        flash(
+            "Registration was successful. "
+            "Login with the ID and password given to you "
+            "and change the password."
+        )
+
         return redirect(url_for("login.login_S"))
+
     return render_template("regiater_student.html")
-
-
