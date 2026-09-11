30e41aa5b95369b5ee9d11ea696d376628e1aec2
Abdul latif <iamabdullatifmo@gmail.com>
2026-09-11 03:55:01 -0700

    render changes

--- school.py
+++ school.py
@@ -65,7 +65,7 @@ app.register_blueprint(admin_bp,url_prefix="")
 app.register_blueprint(admin_update_api, url_prefix="")
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
 app.config["SESSION_PERMANENT"] = False
 
